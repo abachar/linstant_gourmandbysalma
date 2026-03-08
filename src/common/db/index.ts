a3c854fd3ac142404836fd13a93c57833fa8f1de
Abdelhakim Bachar <a.bachar@hotmail.fr>
2026-03-08 19:34:02 +0100

    revert logs

--- src/common/db/index.ts
+++ src/common/db/index.ts
@@ -2,21 +2,12 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { inventory, purchases, sales } from "./schema";
 
 if (!process.env.DATABASE_URL) {
-	console.error("DATABASE_URL environment variable not set");
-	process.exit(1);
+	throw new Error("DATABASE_URL environment variable not set");
 }
 
-console.log("⏳ Tentative de connexion à la base de données...");
 export const db = drizzle(process.env.DATABASE_URL, {
 	schema: { sales, purchases, inventory },
 	logger: process.env.NODE_ENV !== "production",
 });
-
-db.execute("SELECT 1")
-	.then(() => console.log("✅ Drizzle : Connexion établie avec succès !"))
-	.catch((err) => {
-		console.error("❌ Drizzle : Échec de la connexion !", err.message);
-		process.exit(1);
-	});
 
 export { sales, purchases, inventory };
