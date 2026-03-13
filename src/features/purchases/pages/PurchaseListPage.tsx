be64f4eecc32225029399f789fd89669fb88aef5
Abdelhakim Bachar <a.bachar@hotmail.fr>
2026-03-13 16:31:17 +0100

    fix reload after delete

--- src/features/purchases/pages/PurchaseListPage.tsx
+++ src/features/purchases/pages/PurchaseListPage.tsx
@@ -1,33 +1,31 @@
+import { useMutation } from "@common/hooks";
 import { PageLayout } from "@components/layouts";
 import { CardList, EmptyState } from "@components/ui";
-import { useMutation } from "@common/hooks";
 import { useNavigate } from "@tanstack/solid-router";
 import { ShoppingBasket } from "lucide-solid";
-import { type Component, createSignal } from "solid-js";
+import type { Component } from "solid-js";
 import type { FindAllPurchasesReturn } from "../api.functions";
 import { deletePurchaseByIdFn } from "../api.functions";
 import { HeaderUploadButton, PurchaseCardContent, PurchaseListFilter } from "./components";
 
-type Purchase = FindAllPurchasesReturn["purchases"][number];
-
 export const PurchaseListPage: Component<FindAllPurchasesReturn> = (props) => {
 	const navigate = useNavigate();
-	const [purchases, setPurchases] = createSignal<Purchase[]>(props.purchases);
+	const { mutate: deletePurchase } = useMutation({
+		fn: deletePurchaseByIdFn,
+		onSuccess: () => window.location.reload(),
+	});
 
-	const onEditClick = (purchase: Purchase) =>
+	const onEditClick = (purchase: FindAllPurchasesReturn["purchases"][number]) =>
 		navigate({ to: `/purchases/$id/edit`, params: { id: purchase.id } });
 
-	const { mutate: deletePurchase } = useMutation({ fn: deletePurchaseByIdFn });
-	const onDeleteClick = async (purchase: Purchase) => {
-		await deletePurchase({ data: { id: purchase.id } });
-		setPurchases((prev) => prev.filter((p) => p.id !== purchase.id));
-	};
+	const onDeleteClick = (purchase: FindAllPurchasesReturn["purchases"][number]) =>
+		deletePurchase({ data: { id: purchase.id } });
 
 	return (
 		<PageLayout title="Achats" addUrl="/purchases/new" moreActions={<HeaderUploadButton />}>
 			<PurchaseListFilter availableYears={props.availableYears} selectedYear={props.selectedYear} />
 
-			{purchases().length === 0 ? (
+			{props.purchases.length === 0 ? (
 				<EmptyState
 					emptyIcon={<ShoppingBasket />}
 					emptyLabel="Aucun achat trouvé."
@@ -35,7 +33,7 @@ export const PurchaseListPage: Component<FindAllPurchasesReturn> = (props) => {
 					actionLabel="Créer un achat"
 				/>
 			) : (
-				<CardList rows={purchases()} onEditClick={onEditClick} onDeleteClick={onDeleteClick}>
+				<CardList rows={props.purchases} onEditClick={onEditClick} onDeleteClick={onDeleteClick}>
 					{(purchase) => <PurchaseCardContent purchase={purchase} />}
 				</CardList>
 			)}
