7a518a10a0d3550c5c0a4f8fc13112fc514a7f81
Abdelhakim Bachar <a.bachar@hotmail.fr>
2026-03-14 18:06:35 +0100

    add @tanstack/solid-query

--- src/components/layouts/RootComponent.tsx
+++ src/components/layouts/RootComponent.tsx
@@ -1,9 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 import { Outlet } from "@tanstack/solid-router";
 import type { Component } from "solid-js";
 import { RootDocument } from "./RootDocument";
 
+const queryClient = new QueryClient();
+
 export const RootComponent: Component = () => (
-	<RootDocument>
-		<Outlet />
-	</RootDocument>
+	<QueryClientProvider client={queryClient}>
+		<RootDocument>
+			<Outlet />
+		</RootDocument>
+	</QueryClientProvider>
 );
